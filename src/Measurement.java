90ca2c71e94b02c63ce4d5cc35a4fcda5afc29d2
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-17 12:28:39 -0300

    refactor: Change the constants to static.

--- src/Measurement.java
+++ src/Measurement.java
@@ -1,9 +1,9 @@
 public class Measurement {
 
-    private final String[] SI_UNITS = {"s","m","kg","A","K","mol","cd"};
-    private final String[] IMPERIAL_UNITS = {"ft","lib","F"};
-    private final double ft2m = 0.3048;
-    private final double lb2kg = 0.4536;
+    private static final String[] SI_UNITS = {"s","m","kg","A","K","mol","cd"};
+    private static final String[] IMPERIAL_UNITS = {"ft","lib","F"};
+    private static final double ft2m = 0.3048;
+    private static final double lb2kg = 0.4536;
     private String note;
     private double value;
     private String unit;
